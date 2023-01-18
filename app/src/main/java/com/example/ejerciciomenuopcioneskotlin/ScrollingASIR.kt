f96110cd8a68818886aa578c50b8c491e01941e7
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-01-18 19:10:59 +0100

    Terminada

--- app/src/main/java/com/example/ejerciciomenuopcioneskotlin/ScrollingASIR.kt
+++ app/src/main/java/com/example/ejerciciomenuopcioneskotlin/ScrollingASIR.kt
@@ -2,24 +2,23 @@ package com.example.ejerciciomenuopcioneskotlin
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ejerciciomenuopcioneskotlin.databinding.ActivityScrollingAsirBinding
 
 class ScrollingASIR : AppCompatActivity() {
-
+    //Creamos el binding que nos sirve para la vinculacion de vista
     private lateinit var binding: ActivityScrollingAsirBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //Estas lineas hace que se nos vincule las vistas con el fragmento
         binding = ActivityScrollingAsirBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //Vinculacion con el Toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
+        //Poner un titulo al toolbar para que aparezca
         binding.toolbarLayout.title = title
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al boton
+        // flotante con su vista, cuando se pulsa el botón, navegamos a un destino
         binding.fab.setOnClickListener { view ->
             startActivity(Intent(this, MainActivity::class.java))
             finish()
--- app/src/main/java/com/example/ejerciciomenuopcioneskotlin/ScrollingDAM.kt
+++ app/src/main/java/com/example/ejerciciomenuopcioneskotlin/ScrollingDAM.kt
@@ -2,24 +2,23 @@ package com.example.ejerciciomenuopcioneskotlin
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ejerciciomenuopcioneskotlin.databinding.ActivityScrollingDamBinding
 
 class ScrollingDAM : AppCompatActivity() {
-
+    //Creamos el binding que nos sirve para la vinculacion de vista
     private lateinit var binding: ActivityScrollingDamBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //Estas lineas hace que se nos vincule las vistas con el fragmento
         binding = ActivityScrollingDamBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //Vinculacion con el Toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
+        //Poner un titulo al toolbar para que aparezca
         binding.toolbarLayout.title = title
+        //Definimos un escuchador mediante el método setOnClickListener que esta asociado al boton
+        // flotante con su vista, cuando se pulsa el botón, navegamos a un destino
         binding.fab.setOnClickListener { view ->
             startActivity(Intent(this, MainActivity::class.java))
             finish()
